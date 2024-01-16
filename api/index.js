55f338675767a373c1671a1ffcf263535f856489
Tanaya <tanayamohanty0909@gmail.com>
2024-01-16 21:20:52 +0530

    connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config();
+// console.log(process.env.MONGO_URL);
+
+mongoose.connect(process.env.MONGO_URL);
 
 app.get('/test', (req, res)=>{
     res.json('test ok');
 });
 
-app.post('/egister', (req, res)=>{
+app.post('/register', (req, res)=>{
 
 });
 
 app.listen(5000);
+
