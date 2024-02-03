6b159cdf73e9352e92db4ad59ceedac4d5cfe4cb
Tanaya <tanayamohanty0909@gmail.com>
2024-02-03 11:25:31 +0530

    Sign in and Sign Up functionality added

---
+++ client/src/RegisterAndLoginForm.jsx
@@ -0,0 +1,58 @@
+import { useContext, useState } from "react";
+import axios from "axios"; 
+import { UserContext } from "./userContext";
+
+export default function RegisterAndLoginForm(){
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoggedin, setIsloggedin] = useState('register');
+    const {setUsername:setLoggedInUsername, setId} = useContext(UserContext);
+    async function handleResponse(ev){
+        ev.preventDefault();
+        const url = isLoggedin === 'register' ? 'register' : 'login';
+        const{data} = await axios.post(url, {username, password});
+        setLoggedInUsername(username);
+        setId(data.id);
+    }
+    return(
+        <div className="bg-pink-200 h-screen flex items-center">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 w-80 mx-auto" onSubmit={handleResponse}>
+                <label class="block text-gray-700 text-sm font-bold mb-2 mt-2" for="username">Username</label>
+                <input value={username} 
+                onChange={ev=>setUserName(ev.target.value)} 
+                type="text" placeholder="username" className="block bg-pink-50 w-full rounded p-2 mb-4">
+                </input>
+
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+                <input value={password}
+                onChange={ev=>setPassword(ev.target.value)} 
+                type="password" placeholder="password" className="block bg-pink-50 w-full rounded p-2 mb-3">
+                </input>
+                
+                <button className="bg-violet-500 hover:bg-violet-600 text-white font-bold rounded block mt-6 mx-auto p-2 py-2 mb-1">
+                    {isLoggedin === 'register' ? 'Sign Up' : 'Sign In'}
+                </button>
+
+                <div className="text-center mt-4 text-gray-700">
+                    {isLoggedin ==='register' && (
+                        <div>
+                        Already registered?
+                        <button className="mx-1 text-violet-600" onClick={()=> setIsloggedin('login')}>
+                        Sign In here!
+                        </button>
+                        </div>
+                    )}
+
+                    {isLoggedin ==='login' && (
+                        <div>
+                        Don't have an account?
+                        <button className="mx-1 text-violet-600" onClick={()=> setIsloggedin('register')}>
+                        Sign Up here!
+                        </button>
+                        </div>
+                    )}
+                </div>
+            </form>
+        </div>
+    );
+}
