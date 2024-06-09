aa416a177eea9ea585a7a653258a7229599d0a23
Tanaya <tanayamohanty0909@gmail.com>
2024-06-09 12:44:39 +0530

    UI handled for sending messages

--- client/src/ChatWindow.jsx
+++ client/src/ChatWindow.jsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "./userContext";
 
 export default function ChatWindow(){
     const [ws, setWs] = useState(null); 
     const [onlinePeople, setOnlinePeople] = useState({});
+    const [selectedUserId, setSelectedUserId] = useState(null);
+    const {username, id} = useContext(UserContext); 
+    const [msgtxt, setMsgtxt] = useState('');
+
     useEffect(()=>{
         const ws = new WebSocket('ws://localhost:5000');
-        // const ws = new WebSocket("ws://" + location.host + "/");
         setWs(ws);
         ws.addEventListener('message', handleMessage);
     }, []);
@@ -24,29 +28,60 @@ export default function ChatWindow(){
         if('online' in messageData){
             showOnlinePeople(messageData.online);
         }
-
     }
+
+    function sendMsg(ev){
+        ev.preventDefault();
+        console.log('sending msg');
+        ws.send(JSON.stringify({
+            message:{
+                recipient: selectedUserId,
+                text: msgtxt,
+            }
+        }));
+    }
+
+    const otherContacts = {...onlinePeople};
+    delete otherContacts[id];
+
     return (
         <div className="flex h-screen">
-            <div className="bg-pink-200 w-1/4 p-2">
-                <div className="text-pink-600 font-bold">ChimeIn!</div>
-                {Object.keys(onlinePeople).map(userId => (
-                    <div className="border-b border-white-400 py-2">
-                        {onlinePeople[userId]}
+            <div className="bg-pink-200 w-1/4">
+                <div className="text-pink-600 font-bold p-4">ChimeIn!</div>
+            
+                {Object.keys(otherContacts).map(userId => (
+                    <div key={userId} onClick={()=>setSelectedUserId(userId)} 
+                        className={"border-b border-white-400 py-3 px-3 cursor-pointer " + (userId === selectedUserId ? "bg-green-100" : "")}>
+                        {otherContacts[userId]}
                     </div>
                 ))}
             </div>
 
             <div className="flex flex-col bg-white w-3/4">
-                <div className="flex-grow">messages with selected person</div>
-                <div className="flex gap-2 mx-1">
-                    <input type="text" className="bg-gray-100 rounded-sm flex-grow" placeholder="type anything"></input>
-                    <button className="p-2 text-pink-400 border rounded-sm border-pink-100">
+                <div className="flex-grow">
+                    {!selectedUserId && (
+                        <div className="flex flex-grow h-full items-center justify-center">
+                            <div className="text-slate-400">Please select a contact!</div>
+                        </div>
+                    )
+
+                    }
+                </div>
+
+                {!!selectedUserId && (
+                    <form className="flex gap-2 mx-1" onSubmit={sendMsg}>
+                    <input type="text" className="bg-gray-100 rounded-sm flex-grow" 
+                    value={msgtxt} 
+                    onChange={ev=> setMsgtxt(ev.target.value)} 
+                    placeholder="type anything"></input>
+                    <button type="submit" className="p-2 text-pink-400 border rounded-sm border-pink-100">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-  <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
+                            <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
                         </svg>
                 </button>
-                </div>
+                </form>
+                )}
+                
             </div>
 
         </div>
